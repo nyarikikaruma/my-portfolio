f331661f2d658991c08e7fe298e2491d2d5fd9ff
nyarikikaruma <nyarikikaruma@gmail.com>
2024-09-20 17:56:29 +0300

    New changes added

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,23 +1,22 @@
 <template>
- <v-row class="ma-6">
-            <v-col cols="6">
-                <p class="text-subtitle1 text-blue-darken-4">Hello I'm</p>
-                <p class="text-h1 font-weight-bold">George</p>
-                <p class="text-h1 font-weight-bold">Karuma</p>
-                <p class="text-h4">A Passionate Frontend Developer</p>
-                <v-btn color="blue-darken-4" class="mt-4">Say Hello</v-btn>
-            </v-col>
-            <v-col cols="6">
-                <v-img rounded="circle" height="100%" src="/assets/img/profile.png">
+    <v-row style="background-color: hsl(247.2deg 51.02% 19.22%);" class="mt-6 mb-6">
+        <v-col cols="6">
+            <p class="text-subtitle1 text-blue-darken-4">Hello I'm</p>
+            <p class="text-h1 font-weight-bold text-white">George</p>
+            <p class="text-h1 font-weight-bold text-white">Karuma</p>
+            <p class="text-h4">A Passionate Frontend Developer</p>
+            <v-btn color="blue-darken-4" class="mt-4">Say Hello</v-btn>
+        </v-col>
+        <v-col cols="6">
+            <v-img rounded="circle" height="100%" src="/assets/img/profile.png"
+                class="animate__animated animate__fadeInDown animate_slow">
 
-                </v-img>
-            </v-col>
-        </v-row>
+            </v-img>
+        </v-col>
+    </v-row>
 </template>
 
 <script setup>
 </script>
 
-<style>
-
-</style>
+<style></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css' }]
+    }
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['vuetify-nuxt-module']
