e5629a9ceff4dfad8b0a45dd6cdc73eb3cc01134
nyarikikaruma <nyarikikaruma@gmail.com>
2024-09-27 17:09:25 +0300

    responsiveness added

--- pages/index.vue
+++ pages/index.vue
@@ -61,33 +61,39 @@
         <!-- INTRODUCTION -->
         <div class="mt-12 ml-8 text-white">
             <div class="text-slate-400 text-lg">INTRODUCTION</div>
-            <div class="text-5xl font-extrabold text-gray-50 mb-8 mt-2">Overview</div>
+            <div class="text-5xl font-extrabold text-gray-50 mb-8 mt-2">Summary</div>
             <div class="w-1/2 text-white text-lg">Greetings! I'm George Karuma, a passionate and experienced Full
                 Stack Developer
                 with
                 over 3 years of
                 industry experience. My expertise lies in crafting robust, scalable, and user-centric web applications
                 using cutting-edge technologies.</div>
-            <div class="flex mt-3">
+            <div class="text-slate-400 text-base">
+                <p>Phone: +254714837245</p>
+                <p>Email: nyarikikaruma@gmail.com</p>
+            </div>
+            <div class="flex flex-col md:flex-row mt-3 space-y-3 md:space-y-0 md:space-x-3">
                 <button type="button"
-                    class="text-white bg-purple-700 mr-3 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
-                    <a href="https://www.linkedin.com/in/george-karuma/" target="_blank">LinkedIn</a></button>
+                    class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+                    <a href="https://www.linkedin.com/in/george-karuma/" target="_blank">LinkedIn</a>
+                </button>
                 <button type="button"
-                    class="text-white bg-purple-700 mr-3 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
-                    <a href="https://www.github.com/nyarikikaruma/" target="_blank">Github Profile</a></button>
-                <button type="button"
-                    class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+                    class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+                    <a href="https://www.github.com/nyarikikaruma/" target="_blank">Github Profile</a>
+                </button>
+                <button
+                    class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
                     <a target="_blank" href="/pdf/resume.pdf" width="1440px" height="900px">Download Resume</a>
                 </button>
-
             </div>
+
         </div>
         <!-- TECH STACK -->
-        <div class="m-10 grid grid-cols-1 md:grid-cols-4 justify-items-center">
+        <div class="m-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 justify-items-center">
             <div
                 class="w-64 mb-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex flex-col items-center p-10">
-                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/assets/img/vue.svg" alt="Bonnie image" />
+                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/assets/img/vue.svg" alt="Vue image" />
                     <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Vue Js</h5>
                     <span class="text-sm text-gray-500 dark:text-gray-400">Developer</span>
                 </div>
@@ -95,7 +101,7 @@
             <div
                 class="w-64 mb-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex flex-col items-center p-10">
-                    <img class="w-24 h-24 mb-3 shadow-lg" src="/assets/img/laravel.svg" alt="Bonnie image" />
+                    <img class="w-24 h-24 mb-3 shadow-lg" src="/assets/img/laravel.svg" alt="Laravel image" />
                     <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Laravel</h5>
                     <span class="text-sm text-gray-500 dark:text-gray-400">Developer</span>
                 </div>
@@ -103,7 +109,7 @@
             <div
                 class="w-64 mb-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex flex-col items-center p-10">
-                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/assets/img/node.svg" alt="Bonnie image" />
+                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/assets/img/node.svg" alt="Node image" />
                     <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Node Js</h5>
                     <span class="text-sm text-gray-500 dark:text-gray-400">Developer</span>
                 </div>
@@ -112,36 +118,109 @@
                 class="w-64 mb-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex flex-col items-center p-10">
                     <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/assets/img/tailwind.svg"
-                        alt="Bonnie image" />
-                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Tailwind Css</h5>
+                        alt="Tailwind image" />
+                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Tailwind CSS</h5>
                     <span class="text-sm text-gray-500 dark:text-gray-400">Developer</span>
                 </div>
             </div>
         </div>
+
         <!-- WORK EXPERIENCE -->
-        <div>
+        <div class="mt-10">
             <div class="flex justify-center">
-                <div>
-                    <div class="text-slate-400 text-lg flex justify-center">WHAT I HAVE DONE SO FAR</div>
-                    <div class="font-extrabold text-5xl text-white">Work Experience.</div>
+                <div class="text-center">
+                    <div class="text-slate-400 text-lg">WHAT I HAVE DONE SO FAR</div>
+                    <div class="font-extrabold text-5xl text-white">Work Experience</div>
                 </div>
             </div>
-            <div>
 
-
+            <!-- First Row -->
+            <div class="grid grid-cols-1 md:grid-cols-12 gap-4 mt-5">
                 <div
-                    class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    class="md:col-start-3 md:col-span-3 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                     <a href="#">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Senior Frontend
                             Developer</h5>
                     </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise
-                        technology acquisitions of 2021 so far, in reverse chronological order.</p>
-
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological
+                        order.
+                    </p>
                 </div>
+                <div class="hidden md:flex justify-center items-center md:col-span-1">
+                    <div class="w-2 bg-slate-300 h-64"></div>
+                </div>
+                <div class="md:col-span-2">
+                    <p class="text-slate-400 text-lg"><span><a href="https://nathandigital.com/">Nathan
+                                Digital</a></span> August 2024 - Present</p>
+                </div>
+            </div>
 
+            <!-- Second Row -->
+            <div class="grid grid-cols-1 md:grid-cols-12 gap-4 mt-5">
+                <div class="md:col-span-2 md:col-start-4">
+                    <p class="text-slate-400 text-lg"><span><a href="https://zuri-labs.vercel.app/">Zuri Labs
+                                Africa</a></span> Feb 2024 - Aug 2024</p>
+                </div>
+                <div class="hidden md:flex justify-center items-center md:col-span-1">
+                    <div class="w-2 bg-slate-300 h-64"></div>
+                </div>
+                <div
+                    class="md:col-span-3 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Fullstack
+                            Developer</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        Developed and maintained full stack web applications using Laravel, Vue.js, and Nuxt.js.
+                    </p>
+                </div>
+            </div>
+
+            <!-- Third Row -->
+            <div class="grid grid-cols-1 md:grid-cols-12 gap-4 mt-5">
+                <div
+                    class="md:col-start-3 md:col-span-3 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Fullstack
+                            Developer</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        Developed and maintained full stack web applications using Laravel, Vue.js, and Nuxt.js.
+                        Collaborated with UI/UX developers to craft user-friendly applications.
+                    </p>
+                </div>
+                <div class="hidden md:flex justify-center items-center md:col-span-1">
+                    <div class="w-2 bg-slate-300 h-64"></div>
+                </div>
+                <div class="md:col-span-2">
+                    <p class="text-slate-400 text-lg"><span><a href="https://goatmentor.tech/">Goatmentor
+                                Technologies</a></span> April 2022 - Feb 2024</p>
+                </div>
+            </div>
+
+            <!-- Fourth Row -->
+            <div class="grid grid-cols-1 md:grid-cols-12 gap-4 mt-5">
+                <div class="md:col-span-2 md:col-start-4">
+                    <p class="text-slate-400 text-lg"><span>Zalego Institute of Technology</span> May 2019 - Aug 2019
+                    </p>
+                </div>
+                <div class="hidden md:flex justify-center items-center md:col-span-1">
+                    <div class="w-2 bg-slate-300 h-64"></div>
+                </div>
+                <div
+                    class="md:col-span-3 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Web Developer
+                            Intern</h5>
+                    </a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                        I created e-commerce applications using Vanilla PHP and gained great knowledge in programming.
+                    </p>
+                </div>
             </div>
         </div>
+
     </div>
 </template>
 
