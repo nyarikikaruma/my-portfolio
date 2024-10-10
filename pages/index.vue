45612c0ba0561c40f732c3d125774189866ffac1
nyarikikaruma <nyarikikaruma@gmail.com>
2024-10-10 09:14:53 +0300

    Jimi

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <div style="background-color: #050816;">
         <nav class="bg-white border-gray-200 dark:bg-gray-900">
             <div class="flex flex-wrap justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="/assets/img/profile.png" class="h-8 rounded-full" alt="Flowbite Logo" />
                     <span
                         class="self-center text-base md:text-2xl font-semibold whitespace-nowrap dark:text-white">George
@@ -33,10 +33,6 @@
                         <li>
                             <a href="#"
                                 class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
                         </li>
                         <li>
                             <a href="#"
